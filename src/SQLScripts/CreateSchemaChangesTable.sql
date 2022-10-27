c67543803754ccbf7a7b3d22fb36a9dd83532aa6
Bowen <592623044@qq.com>
2022-10-27 21:58:46 +1030

    Update CreateSchemaChangesTable.sql

--- src/SQLScripts/CreateSchemaChangesTable.sql
+++ src/SQLScripts/CreateSchemaChangesTable.sql
@@ -1,38 +1,27 @@
 IF NOT EXISTS (SELECT * FROM sys.tables WHERE object_id = OBJECT_ID('dbo.SchemaChanges'))
 BEGIN
     CREATE TABLE [dbo].[SchemaChanges] (
-        [ID]         INT            IDENTITY (1, 1) NOT NULL,
-        [ScriptName] VARCHAR(255)   NOT NULL CONSTRAINT [UC_ScriptName] UNIQUE,
-        [State]      CHAR(1)        NOT NULL CONSTRAINT [DF_SchemaChanges_State] DEFAULT ('A'),
-        [Applied]    DateTime       NOT NULL CONSTRAINT [DF_SchemaChanges_Applied] DEFAULT(CURRENT_TIMESTAMP),
-        [AppliedBy]  VARCHAR(255)   NOT NULL CONSTRAINT [DF_SchemaChanges_AppliedBy] DEFAULT (suser_name()),
-        [Modified]   DateTime,
-        [ModifiedBy] VARCHAR(255),
-        CONSTRAINT [PK_SchemaChanges_ID] PRIMARY KEY CLUSTERED ([ID] ASC),
-        CONSTRAINT [CK_SchemaChanges_State] CHECK ([State] = 'R' OR [State] = 'A'),
+        [ID]                INT             IDENTITY (1, 1) PRIMARY KEY,
+        [ScriptName]        VARCHAR(255)    NOT NULL CONSTRAINT [UQ_ScriptName] UNIQUE,
+        [AppliedAt]         DateTime        NOT NULL CONSTRAINT [DF_SchemaChanges_Applied] DEFAULT(SYSDATETIMEOFFSET()),
+        [ModifiedAt]        DateTime        NOT NULL,
+        [ModifiedBy]        VARCHAR(255)    NOT NULL,
+        CONSTRAINT [PK_SchemaChanges_ID] PRIMARY KEY CLUSTERED ([ID] ASC)
     )
-
-    PRINT 'Created schema change tracking table'
 END
 
-IF NOT EXISTS (SELECT * FROM sys.triggers WHERE object_id = OBJECT_ID(N'[dbo].[TR_SchemaChanges_Update]'))
+IF NOT EXISTS (SELECT * FROM sys.triggers WHERE object_id = OBJECT_ID(N'[TR_SchemaChanges_Update]'))
 BEGIN
-    DECLARE @SQL VARCHAR(MAX)
-
-    SET @SQL = 'CREATE TRIGGER [dbo].[TR_SchemaChanges_Update]
+    CREATE TRIGGER [TR_SchemaChanges_Update]
         ON [dbo].[SchemaChanges]
-        FOR UPDATE
-        AS
-        BEGIN
-            SET NoCount ON
-
-            UPDATE [dbo].[SchemaChanges]
-            SET
-                Modified = GETUTCDATE(),
-                ModifiedBy = SUSER_NAME()
-            FROM [dbo].[SchemaChanges]
-            INNER JOIN inserted ON  [dbo].[SchemaChanges].[ID] = inserted.[ID];
-
-        END'
-    EXECUTE (@SQL)
+        AFTER UPDATE
+    AS
+    BEGIN
+        UPDATE [dbo].[SchemaChanges]
+        SET
+            [ModifiedAt] = SYSDATETIMEOFFSET(),
+            [ModifiedBy] = SUSER_NAME()
+        FROM [dbo].[SchemaChanges]
+        WHERE [ID] in (SELECT [ID] FROM INSERTED)
+    END
 END
